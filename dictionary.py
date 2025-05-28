2e89717043c5b48c8345ce334a107901b2e2c852
Muzammil-2 <muzammil01vivekengg@gmail.com>
2025-05-28 23:42:48 +0530

    stored dic in app

---
+++ dictionary.py
@@ -0,0 +1,13 @@
+info = {
+    "name": "Muzammil",
+     "age": "23",
+     "blood Group": "B+",
+     "subject":{
+         "phy": "44",
+         "Chem": "55",
+         "Math": "77"
+     }
+
+}
+
+print (info)
